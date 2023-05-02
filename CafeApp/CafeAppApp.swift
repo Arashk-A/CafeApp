56b57e951d4bfa958f45fe7517675ea14afb3bbd
zero <m.birang@gmail.com>
2023-05-02 17:15:12 +0330

    Initial Commit

---
+++ CafeApp/CafeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeAppApp.swift
+//  CafeApp
+//
+//  Created by zero on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CafeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
